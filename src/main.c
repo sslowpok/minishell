619461f0af2e0907a5ebfc1285a093d3c842076d
Sheryl Slowpoke <sslowpok@mi-r5.msk.21-school.ru>
2022-04-25 15:39:50 +0300

    main: added init

--- src/main.c
+++ src/main.c
@@ -6,20 +6,43 @@
 /*   By: sslowpok <sslowpok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 13:10:57 by sslowpok          #+#    #+#             */
-/*   Updated: 2022/04/25 15:17:19 by sslowpok         ###   ########.fr       */
+/*   Updated: 2022/04/25 15:39:15 by sslowpok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	main(int __unused argc, char __unused **argv, char __unused **envp)
+void	init_res_words(t_info *info)
 {
-	// if (argc != 1)
-	// {
-	// 	return (1);
-	// }
+	info->reserved_words[0] = "echo";
+	info->reserved_words[1] = "cd";
+	info->reserved_words[2] = "pwd";
+	info->reserved_words[3] = "export";
+	info->reserved_words[4] = "unset";
+	info->reserved_words[5] = "env";
+	info->reserved_words[6] = "exit";
+}
 
-	pipex(argc, argv, envp);
+void	init_info(t_info *info, char **envp)
+{
+	init_res_words(info);
+	info->envp = envp;
+	info->envp_f = 0;
+	info->exit_f = 0;
+	info->status = 0;
+}
+
+
+int	main(int argc, char __unused **argv, char __unused **envp)
+{
+	t_info	info;
+
+	if (argc != 1)
+	{
+		return (1);
+	}
+	
+	init_info(&info, envp);
 	
 	return (0);
 }
