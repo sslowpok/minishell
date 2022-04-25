6252a757767dba34185d6a3df9695b475a71196b
Sheryl Slowpoke <sslowpok@mi-r5.msk.21-school.ru>
2022-04-25 13:56:16 +0300

    minihsell.h: added libs

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,5 +3,13 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <errno.h>
+
+# include "../libft/libft.h"
+
+
 
 #endif
