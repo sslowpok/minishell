0ed330216e7de1975212259eb5744ee4f7df9d3d
Chelsey Overandout <coverand@mi-g5.msk.21-school.ru>
2022-05-14 17:40:15 +0300

    header 4 builtins added

---
+++ includes/builtins.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coverand <coverand@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 17:25:17 by coverand          #+#    #+#             */
+/*   Updated: 2022/05/14 17:30:09 by coverand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTINS_H
+# define BUILTINS_H
+
+# include "minishell.h"
+
+int		ft_echo(char **args);
+int		ft_env(char **args, t_llist *envp);
+
+#endif
