be65732a171366ee7724cfcc302249055bd9d9c6
Chelsey Overandout <coverand@mi-r3.msk.21-school.ru>
2022-05-18 16:47:06 +0300

    exit handles lli

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: coverand <coverand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 17:25:17 by coverand          #+#    #+#             */
-/*   Updated: 2022/05/18 16:04:16 by coverand         ###   ########.fr       */
+/*   Updated: 2022/05/18 16:45:08 by coverand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,19 @@
 # define BUILTINS_H
 
 # include "minishell.h"
+# include "parser.h"
 # include "envp_parser.h"
 
-# define LLI_PLUS   (unsigned long long)9223372036854775807
-# define LLI_MINUS  9223372036854775808
+# define LLI_PLUS   9223372036854775807
 
-void	ft_echo(char **args);
-void	ft_env(char **args, t_llist *envp);
-void	ft_pwd(char **args, t_llist *envp);
-void	ft_cd(char **args, t_llist *envp);
-void	ft_exit(char **args, t_llist *envp);
+void		ft_echo(char **args);
+void		ft_env(char **args, t_llist *envp);
+void		ft_pwd(char **args, t_llist *envp);
+void		ft_cd(char **args, t_llist *envp);
+void		ft_exit(char **args, t_llist *envp);
+//int			ft_isspace(char s);
+int			ft_get_args_count(char **args);
+int			ft_check_digit(char *str);
+void		ft_handle_lli(char *str);
+long long	ft_get_num(char *str);
 #endif
--- src/builtins_exit.c
+++ src/builtins_exit.c
@@ -6,7 +6,7 @@
 /*   By: coverand <coverand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 14:54:44 by coverand          #+#    #+#             */
-/*   Updated: 2022/05/18 16:05:38 by coverand         ###   ########.fr       */
+/*   Updated: 2022/05/18 16:43:51 by coverand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,76 +50,12 @@ exit
 echo $? -> 1
 */
 
-static int	ft_isspace(char s)
-{
-	if (s == ' ' || s == '\t' || s == '\n' || \
-	s == '\v' || s == '\f' || s == '\r')
-		return (1);
-	return (0);
-}
-
-int	ft_get_args_count(char **args)
-{
-	int	i;
-
-	i = 0;
-	while (args[i])
-		i++;
-	return (i);
-}
-
-int	ft_check_digit(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\t')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while (str[i])
-	{
-		if (str[i] < '0' || str[i] > '9')
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-int	ft_get_num(char *str)
-{
-	unsigned long long	num;
-	int					i;
-	int					sign;
-	
-	i = 0;
-	sign = 1;
-	num = 0;
-	while (ft_isspace(str[i]))
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i++] == '-')
-			sign = -1;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		if (num > LLI_MINUS && sign == -1)
-			return (0);
-		if (num >)
-			return (-1);
-		num = num * 10 + str[i++] - '0';
-	}
-	return (sign * num);
-
-}
-
 /*
 Source => 
 https://stackoverflow.com/questions/11720656/
 modulo-operation-with-negative-numbers
 */
-int	ft_get_return(int n)
+int	ft_get_return(long long n)
 {
 	return ((n % 256 + 256) % 256);
 }
@@ -132,7 +68,7 @@ void	ft_exit(char **args, t_llist __unused *envp)
 	if (count == 1)
 		exit(1);
 	if (count == 2 && !ft_check_digit(args[1]))
-		exit(ft_get_return(ft_atoi(args[1])));
+		exit(ft_get_return(ft_get_num(args[1])));
 	if (ft_check_digit(args[1]))
 	{
 		ft_putendl_fd("exit", 1);
